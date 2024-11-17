a87495b01ae43e83a2faabb01f85737e61bd2385
happyNight <66168852+HappyNBoy@users.noreply.github.com>
2024-11-17 18:57:53 +0200

    jwt stuff

--- jwt_util.py
+++ jwt_util.py
@@ -10,18 +10,20 @@ is_dev_version = "REPL_ID" in os.environ
 
 SECRET_KEY = os.environ["jwt_secret"]
 
+instance = JWT()
+
 def create_jwt_token(data):
     payload = {
         "exp": datetime.datetime.utcnow() + datetime.timedelta(days=1),
         "iat": datetime.datetime.utcnow(),
         "data": data
     }
-    token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
+    token = instance.encode(payload, SECRET_KEY, algorithm="HS256")
     return token
 
 def verify_jwt_token(token):
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        payload = instance.decode(token, SECRET_KEY, algorithms=["HS256"])
         return payload["data"]
     except jwt.ExpiredSignatureError:
         # Token has expired
